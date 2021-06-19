9619a1214effb89470262efba66493bf947033e2
Juanda <danielalejandre1050@yahoo.com>
2021-06-18 21:34:58 -0500

    Configurando Navbar

---
+++ Gran_Salto/fronted/src/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const home = () => {
+    return (
+        <div className = "container"> 
+            <h1 className = "text-center" style = { { paddinTop: "30px" } }>
+                Home
+            </h1>
+        </div>
+    )
+}
+
+export default home
